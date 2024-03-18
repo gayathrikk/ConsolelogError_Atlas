7fe21a939c84645905c799e3afa8af67ab30591e
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 09:38:19 +0530

    Atlas_consolelog.java

--- src/test/java/Error/Console_log/Atlas_consolelog.java
+++ src/test/java/Error/Console_log/Atlas_consolelog.java
@@ -36,7 +36,7 @@ public class Atlas_consolelog {
 		{
 			
 			  DesiredCapabilities dc = DesiredCapabilities.chrome();
-		        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+		        URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		        driver = new RemoteWebDriver(url, dc);
 		
 		       
